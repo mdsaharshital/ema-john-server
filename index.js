116175624732e7c0020d0a1fe08a9a8fd17ed024
mdsaharshital <mdsaharshitol@gmail.com>
2022-04-30 01:04:10 +0600

    new heroku app

--- index.js
+++ index.js
@@ -51,7 +51,6 @@ async function run() {
       const query = { _id: { $in: ids } };
       const cursor = productCollection.find(query);
       const products = await cursor.toArray();
-      console.log(keys);
       res.send(products);
     });
   } finally {
